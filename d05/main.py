6cad52aaff65bc7b9112ab893660a21d082a436b
Gustav Sörnäs <gustav@sornas.net>
2024-12-10 11:06:52 +0100

    solve 5-2

--- d05/main.py
+++ d05/main.py
@@ -1,3 +1,4 @@
+import functools
 import sys
 
 
@@ -5,8 +6,6 @@ def main():
     print("⋆꙳•̩̩͙❅*̩̩͙‧͙   Advent of Code 2024  ‧͙*̩̩͙❆ ͙͛ ˚₊⋆\n")
     inp = sys.stdin.read().strip()
     rules, updates = inp.split("\n\n")
-
-    print(repr(rules), repr(updates))
 
     rules_order = {}
     rules_order_inv = {}
@@ -26,10 +25,11 @@ def main():
             return False
         return True
 
+    updates = [list(map(int, update.split(","))) for update in updates.split("\n")]
+
     s = 0
     wrong = []
-    for update in updates.split("\n"):
-        update = list(map(int, update.split(",")))
+    for update in updates:
         valid = True
         for i, a in enumerate(update):
             for b in update[i+1:]:
@@ -43,27 +43,11 @@ def main():
 
     s2 = 0
     for update in wrong:
-        correct = []
-        left = set(update)
-        while left:
-            added_anything = False
-            for a in left:
-                valid_to_add = True
-                for b in left:
-                    if not valid_order(a,b):
-                        valid_to_add = False
-                        break
-                if valid_to_add:
-                    correct.append(a)
-                    left.remove(a)
-                    added_anything = True
-                    break
-            assert added_anything
-        print(wrong, correct)
+        update.sort(key=functools.cmp_to_key(lambda a,b: 0 if a == b else -1 if b in rules_order.get(a, []) else 1))
+        s2 += update[len(update)//2]
 
     print(1, s)
     print(2, s2)
-
 
 
 main()
