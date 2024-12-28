443dbc097a7ed647710ea832c9aa10db6429c1f2
Gustav Sörnäs <gustav@sornas.net>
2024-12-28 20:28:45 +0100

    21-1

--- d21/main.py
+++ d21/main.py
@@ -1,40 +1,123 @@
-import functools
+from copy import copy
+from dataclasses import dataclass
+from typing import Any, ClassVar
+import heapq
 import sys
+
+UP = 1
+LEFT = 2
+DOWN = 3
+RIGHT = 4
+PRESS = 10
+
+
+
+class Arm:
+    keys: ClassVar[dict[tuple[int, int], Any]]
+
+    def __init__(self, pos):
+        self.pos = pos
+
+    def move(self, d):
+        test = copy(self)
+        dx = dy = 0
+        if d == UP:
+            dy = -1
+        elif d == DOWN:
+            dy = 1
+        elif d == RIGHT:
+            dx = 1
+        elif d == LEFT:
+            dx = -1
+        x, y = test.pos
+        x += dx
+        y += dy
+        if (x, y) in test.keys:
+            test.pos = (x, y)
+            return test
+        return None
+
+    def __lt__(self, other):
+        return self.pos.__lt__(other.pos)
+
+class Numeric(Arm):
+    keys = {
+        (0,0): "7",
+        (1,0): "8",
+        (2,0): "9",
+        (0,1): "4",
+        (1,1): "5",
+        (2,1): "6",
+        (0,2): "1",
+        (1,2): "2",
+        (2,2): "3",
+        (1,3): "0",
+        (2,3): "A"
+    }
+
+    def __init__(self):
+        super().__init__((2, 3))
+
+class Directional(Arm):
+    keys = {
+        (1,0): UP,  # up
+        (2,0): PRESS, # A
+        (0,1): LEFT,  # left
+        (1,1): DOWN,  # down
+        (2,1): RIGHT,  # right
+    }
+
+    def __init__(self):
+        super().__init__((2, 0))
 
 def main():
     inp = sys.stdin.read().strip().splitlines()
 
-    numeric_pos_to_num = {
-        (0,0): 7,
-        (1,0): 8,
-        (2,0): 9,
-        (0,1): 4,
-        (1,1): 5,
-        (2,1): 6,
-        (0,2): 1,
-        (1,2): 2,
-        (2,2): 3,
-        (1,3): 0,
-        (2,3): 10 # A
-    }
-    numeric_num_to_pos = {kv[1]:kv[0] for kv in numeric_pos_to_num.items()}
+    s = 0
+    for target in inp:
+        q = [(0, "", Directional(), Directional(), Numeric(), "")]
+        seen = set()
+        while q:
+            cost, code, d1, d2, n, hist = heapq.heappop(q)
+            if len(code) > len(target):
+                continue
+            if code == target:
+                print(target, cost, hist)
+                s += int(target[:-1]) * cost
+                break
+            if (code, d1.pos, d2.pos, n.pos) in seen:
+                continue
+            seen.add((code, d1.pos, d2.pos, n.pos))
 
-    dir_pos_to_dir = {
-        (1,0): 1,  # up
-        (2,0): 10, # A
-        (0,1): 2,  # left
-        (1,1): 3,  # down
-        (2,1): 4,  # right
-    }
+            def try_move(arm: Arm, d, s):
+                if (new_d := arm.move(d)) is not None:
+                    heapq.heappush(q, (cost + 1, code, new_d, copy(d2), copy(n), hist + s))
 
-    @functools.cache
-    def moves_to_input(num_cur, num_target, dir1_cur, dir1_target):
-        nx, ny = numeric
-        tx, ty = numeric_num_to_pos[target]
-        return abs(nx-tx) + abs(ny-ty) + 1
+            # try to move
+            try_move(d1, UP, "^")
+            try_move(d1, DOWN, "v")
+            try_move(d1, LEFT, "<")
+            try_move(d1, RIGHT, ">")
+
+            # if the first dir is looking at "A"
+            if (d1_press := Directional.keys[d1.pos]) == PRESS:
+                # if the next dir is also looking at "A"
+                if (d2_press := Directional.keys[d2.pos]) == PRESS:
+                    # press inner
+                    if (code_press := Numeric.keys[n.pos]) == target[len(code)]:
+                        heapq.heappush(q, (cost + 1, code + code_press, copy(d1), copy(d2), copy(n), hist + "A"))
+                else:
+                    # move n
+                    if (new_n := n.move(d2_press)) is not None:
+                        heapq.heappush(q, (cost + 1, code, copy(d1), copy(d2), new_n, hist + "A"))
+            else:
+                # move d2
+                if (new_d2 := d2.move(d1_press)) is not None:
+                    heapq.heappush(q, (cost + 1, code, copy(d1), new_d2, copy(n), hist + "A"))
+
 
     print("⋆꙳•̩̩͙❅*̩̩͙‧͙   Advent of Code 2024  ‧͙*̩̩͙❆ ͙͛ ˚₊⋆\n")
 
-    print(moves_to_input(numeric_num_to_pos[10], 5))
+    print(s)
 
 main()
