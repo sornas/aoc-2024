c06418266655668a2933c79a032db6383f8ba14b
Gustav Sörnäs <gustav@sornas.net>
2024-12-30 17:41:16 +0100

    FINISH

--- d21/main2.py
+++ d21/main2.py
@@ -27,82 +27,48 @@ dirpad = {
 }
 dirpad_inv = {v:k for k, v in dirpad.items()}
 
-# @cache
-def move_dir(fr, to, pad):
-    if pad == 0:
+def _move(x1, y1, x2, y2, invalid, dirpads):
+    dx = x2 - x1
+    dy = y2 - y1
+    dirx = "-" if dx == 0 else "<" if dx < 0 else ">"
+    diry = "-" if dy == 0 else "^" if dy < 0 else "v"
+
+    x_first_valid = (x2, y1) != invalid
+    y_first_valid = (x1, y2) != invalid
+
+    def move_seq(seq):
+        return sum(move_dir(a, b, dirpads - 1) for a, b in zip("A" + seq, seq))
+
+    # lazy evaluation
+
+    # x first
+    def xy():
+        return move_seq(dirx * abs(dx) + diry * abs(dy) + "A")
+
+    # y first
+    def yx():
+        return move_seq(diry * abs(dy) + dirx * abs(dx) + "A")
+
+    if not x_first_valid:
+        return yx()
+    elif not y_first_valid:
+        return xy()
+    else:
+        return min(xy(), yx())
+
+@cache
+def move_dir(fr, to, dirpads):
+    if dirpads == 0:
         return 1
+    return _move(*dirpad_inv[fr], *dirpad_inv[to], dirpad_inv[None], dirpads)
 
-    x1, y1 = dirpad_inv[fr]
-    x2, y2 = dirpad_inv[to]
-    dx = x2 - x1
-    dy = y2 - y1
-    dirx = "<" if dx < 0 else ">"
-    diry = "^" if dy < 0 else "v"
-
-    x_first_valid = dirpad[(x2, y1)] is not None
-    y_first_valid = dirpad[(x1, y2)] is not None
-
-    def xy():
-        seq = dirx * dx + diry * dy + "A"
-        return sum(move_dir(a, b, pad - 1) for a, b in zip("A" + seq, seq))
-
-    def yx():
-        seq = dirx * dx + diry * dy + "A"
-        return sum(move_dir(a, b, pad - 1) for a, b in zip("A" + seq, seq))
-
-    if not x_first_valid:
-        return yx()
-    elif not y_first_valid:
-        return xy()
-    else:
-        tx = xy()
-        ty = yx()
-        return min(tx, ty)
-
-# TODO: same move_num + move_dir?
-# TODO: return (presses, string) for debug
-
-# @cache
-def move_num(fr, to, pad):
-    print("num", fr, to)
-    x1, y1 = numpad_inv[fr]
-    x2, y2 = numpad_inv[to]
-    dx = x2 - x1
-    dy = y2 - y1
-    dirx = "<" if dx < 0 else ">"
-    diry = "^" if dy < 0 else "v"
-
-    x_first_valid = numpad[(x2, y1)] is not None
-    y_first_valid = numpad[(x1, y2)] is not None
-
-    def xy():
-        seq = dirx * dx + diry * dy + "A"
-        return sum(move_dir(a, b, pad) for a, b in zip("A" + seq, seq))
-
-    def yx():
-        seq = dirx * dx + diry * dy + "A"
-        return sum(move_dir(a, b, pad) for a, b in zip("A" + seq, seq))
-
-    if not x_first_valid:
-        return yx()
-    elif not y_first_valid:
-        return xy()
-    else:
-        tx = xy()
-        ty = yx()
-        return min(tx, ty)
+@cache
+def move_num(fr, to, dirpads):
+    return _move(*numpad_inv[fr], *numpad_inv[to], numpad_inv[None], dirpads)
 
 
 def main():
     inp = sys.stdin.read().strip()
-
-    t = 0
-    for code in inp.splitlines():
-        print(code)
-        s = 0
-        for a, b in zip("A" + code, code):
-            s += move_num(a, b, 2) + 1
-        t += s
-    print(t)
+    print(sum(int(code[:-1]) * sum(move_num(a, b, 26) for a, b in zip("A" + code, code)) for code in inp.splitlines()))
 
 main()
